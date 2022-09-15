928243825fe8fc3d30e6abbce90cb81ba6c7f674
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2022-09-16 02:51:15 +0300

    the task is solved. Tests is ok.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,8 @@
 // TODO: write your code here
+export default function specialAttack(character) {
+  const { special = [] } = character;
+  return special.map((attack) => ({
+    description: 'описание недоступно',
+    ...attack,
+  }));
+}
